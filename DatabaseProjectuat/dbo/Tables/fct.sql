468878916a20f9f753b2b52eac7999d54f2d5d69
vdthoa <giadinhvohoa9999@gmail.com>
2024-11-20 06:51:31 +0700

    initial commmit

---
+++ DatabaseProjectuat/dbo/Tables/fct.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[fct] (
+    [id]        INT           NULL,
+    [name]      VARCHAR (200) NULL,
+    [isactive]  BIT           NULL,
+    [age]       INT           NULL,
+    [Gender]    INT           NULL,
+    [LastName]  VARCHAR (200) NULL,
+    [FirstName] VARCHAR (200) NULL,
+    [FullName]  INT           NULL,
+    [ID]        INT           NULL
+);
+
+
+GO
+
