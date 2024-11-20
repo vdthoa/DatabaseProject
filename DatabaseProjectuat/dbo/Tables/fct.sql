50bf5c117c5ccf629a0ac39bc0a54af7c5a758f4
vdthoa <giadinhvohoa9999@gmail.com>
2024-11-20 07:03:15 +0700

    Adding Phone Number to fct table and fix create table

--- DatabaseProjectuat/dbo/Tables/fct.sql
+++ DatabaseProjectuat/dbo/Tables/fct.sql
@@ -1,17 +1,26 @@
-CREATE TABLE [dbo].[fct] (
-    [id]        INT           NULL,
-    [name]      VARCHAR (200) NULL,
-    [isactive]  BIT           NULL,
-    [age]       INT           NULL,
-    [Gender]    INT           NULL,
-    [LastName]  VARCHAR (200) NULL,
-    [FirstName] VARCHAR (200) NULL,
-    [FullName]  INT           NULL,
-    [ID]        INT           NULL
-);
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'fct') 
+BEGIN 
+    CREATE TABLE [dbo].[fct] (
+        [id]        INT           NULL,
+        [name]      VARCHAR (200) NULL,
+        [isactive]  BIT           NULL,
+        [age]       INT           NULL,
+        [Gender]    INT           NULL,
+        [LastName]  VARCHAR (200) NULL,
+        [FirstName] VARCHAR (200) NULL,
+        [FullName]  INT           NULL
+    );
+
+END
 
 
 GO
 
-ALTER TABLE [dbo].[fct] ADD [PhoneNumber] VARCHAR(20)
+--//Adding Phone Number to fct table
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'fct' AND COLUMN_NAME = 'PhoneNumber') 
+BEGIN 
+    ALTER TABLE [dbo].[fct] ADD [PhoneNumber] VARCHAR(20)
+END
+
+
 
